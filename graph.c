7467eb642c5e6a207377ecd6f0985fd6ff82f36e
Trobiun <robin.tissot@free.fr>
2018-10-17 12:27:14 +0200

    modif pour graph

--- graph.c
+++ graph.c
@@ -12,18 +12,23 @@ void create_graph(struct graph *self, bool isDirected, size_t nbMaxNodes) {
 	}
 }
 
-void graph_destroy(struct graph *self) {
-	
-}
+// void graph_destroy(struct graph *self) {
+// 	for (size_t i = 0; i < self->nbMaxNodes; i++) {
+// 		
+// 	}
+// 	free(self->adjList);
+// }
 
-void graph_add(struct neighbour *self, size_t nbNodes, int neighbour, int weight) {
+void graph_add_neighbour(struct graph *self, size_t nbNode, int neighbour, int weight) {
 	struct neighbour *add = malloc(sizeof(struct neighbour));
 	neighbour_create(add, neighbour, weight);
-	if (self->adjList[i] == NULL) {
+	if (self->adjList[nbNode] == NULL) {
 		struct neighbour *sentinel = malloc(sizeof(struct neighbour));
 		neighbour_create(sentinel, -1, 0);
-		neighbour_create(add, neighbour, weight);
 		neighbour_add_front(add, sentinel);
-		self->adjList[i] = add;
+		self->adjList[nbNode] = add;
+	}
+	else {
+		neighbour_add_front(self->adjList[nbNode],add);
 	}
 }
--- graph.h
+++ graph.h
@@ -2,18 +2,18 @@
 #define GRAPH_H
 
 #include <stdbool.h>
-#include "array.h"
 #include "neighbour.h"
 
 struct graph {
 	bool isDirected;
 	size_t nbMaxNodes;
-	struct neighbour *adjList;
+	struct neighbour **adjList;
 };
 
 void create_graph(struct graph *self, bool isDirected, size_t nbMaxNodes);
 
 void graph_destroy(struct graph *self);
 
+void graph_add_neighbour(struct graph *self, size_t nbNode, int neighbour, int weight);
 
 #endif // GRAPH_H
